b7686dadc8829410f30341d7c6a4c6e81a82c8c3
Nobuyoshi Nakada <nobu@ruby-lang.org>
2010-08-17 11:19:34 +0900

    add ChupaText class.

--- include/chupatext.h
+++ include/chupatext.h
@@ -3,29 +3,43 @@
  *  Copyright (C) 2010  Nobuyoshi Nakada <nakada@clear-code.com>
  */
 
-#ifndef CHUPATEXT_H
-#define CHUPATEXT_H
+#ifndef CHUPA_TEXT_H
+#define CHUPA_TEXT_H
 
 #include <gio/gio.h>
+#include <glib-object.h>
 
-typedef struct ChupaSwitcher ChupaSwitcher;
-typedef struct ChupaModule ChupaModule;
-typedef int ChupaCallbackFunc(GInputStream *cabra, gpointer arg);
+G_BEGIN_DECLS
+
+#define CHUPATYPE_TEXT            chupa_text_get_type()
+#define CHUPA_TEXT(obj)            G_TYPE_CHECK_INSTANCE_CAST(obj, CHUPA_TYPE_TEXT, ChupaText)
+#define CHUPA_TEXT_CLASS(klass)    G_TYPE_CHECK_CLASS_CAST(klass, CHUPA_TYPE_TEXT, ChupaTextClass)
+#define IS_CHUPA_TEXT(obj)         G_TYPE_CHECK_INSTANCE_TYPE(obj, CHUPA_TYPE_TEXT)
+#define IS_CHUPA_TEXT_CLASS(klass) G_TYPE_CHECK_CLASS_TYPE(klass, CHUPA_TYPE_TEXT)
+#define CHUPA_TEXT_GET_CLASS(obj)  G_TYPE_INSTANCE_GET_CLASS(obj, CHUPA_TYPE_TEXT, ChupaTextClass)
+
+#define CHUPA_TEXT_SIGNAL_READY chupa_text_signal_ready
+extern const char chupa_text_signal_ready[];
+
+typedef struct ChupaText ChupaText;
+typedef struct ChupaTextClass ChupaTextClass;
+
+struct ChupaText
+{
+    GObject parent_instance;
+};
+
+struct ChupaTextClass
+{
+    GObjectClass parent_class;
+    void (*ready)(ChupaText *chupar,
+                  GInputStream *input);
+};
 
 int chupa_init(void *);
 int chupa_cleanup(void);
-ChupaSwitcher *chupa_switcher_new(GInputStream *cabra);
-int chupa_foreach(ChupaSwitcher *chupar, ChupaCallbackFunc *func, gpointer arg);
-
-typedef struct ChupaExtractor ChupaExtractor;
-struct ChupaExtractor {
-    struct {
-        const char *main, *sub;
-    } content_type;
-    GInputStream *(*make_converter_input)(GInputStream *);
-    int (*foreach)(ChupaSwitcher *, GInputStream *, ChupaCallbackFunc *, gpointer);
-};
-
-ChupaExtractor *chupa_extractor_new(ChupaSwitcher *chupar, const char *content_type);
+GType chupa_text_get_type(void) G_GNUC_CONST;
+ChupaText *chupa_text_new(GInputStream *source);
+int chupa_text_foreach(ChupaText *chupar, GCallback *func, gpointer arg);
 
 #endif
