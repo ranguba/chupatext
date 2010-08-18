6e6f849a5b6650bdb2663d975cf5bf73e7b43138
Nobuyoshi Nakada <nobu@ruby-lang.org>
2010-08-18 14:40:08 +0900

    fixed typo.

--- chupatext/chupa_text.c
+++ chupatext/chupa_text.c
@@ -3,7 +3,7 @@
  *  Copyright (C) 2010  Nobuyoshi Nakada <nakada@clear-code.com>
  */
 
-#include <chupatext.h>
+#include "chupatext/chupa_text.h"
 
 G_DEFINE_TYPE(ChupaText, chupatext, G_TYPE_OBJECT)
 
@@ -17,7 +17,7 @@ enum {
 static gint signals[LAST_SIGNAL] = {0};
 
 static void
-chupatext_class_init(ChupaTextClass *klass)
+chupa_text_class_init(ChupaTextClass *klass)
 {
     GObjectClass *gobject_class = G_OBJECT_CLASS(klass);
     /*GTypeModuleClass *type_module_class = G_TYPE_MODULE_CLASS(klass);*/
@@ -30,6 +30,6 @@ chupatext_class_init(ChupaTextClass *klass)
                      G_SIGNAL_RUN_LAST,
                      G_STRUCT_OFFSET(ChupaTextClass, ready),
                      NULL, NULL,
-                     g_cclosure_marshal_VOID__OBJECT,__marshal_VOID__OBJECT,
+                     g_cclosure_marshal_VOID__OBJECT,
                      G_TYPE_NONE, 1, G_TYPE_INPUT_STREAM);
 }
