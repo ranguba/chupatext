db8e9b6d9e19c3566db162c99d7769957c1ea432
Nobuyoshi Nakada <nobu@ruby-lang.org>
2010-08-18 14:41:01 +0900

    update interface.

--- chupatext/chupa_text.h
+++ chupatext/chupa_text.h
@@ -18,8 +18,8 @@ G_BEGIN_DECLS
 #define IS_CHUPA_TEXT_CLASS(klass) G_TYPE_CHECK_CLASS_TYPE(klass, CHUPA_TYPE_TEXT)
 #define CHUPA_TEXT_GET_CLASS(obj)  G_TYPE_INSTANCE_GET_CLASS(obj, CHUPA_TYPE_TEXT, ChupaTextClass)
 
-#define CHUPA_TEXT_SIGNAL_READY chupa_text_signal_ready
-extern const char chupa_text_signal_ready[];
+#define CHUPA_TEXT_SIGNAL_DECOMPOSED chupa_text_signal_decomposed
+extern const char chupa_text_signal_decomposed[];
 
 typedef struct ChupaText ChupaText;
 typedef struct ChupaTextClass ChupaTextClass;
@@ -32,12 +32,11 @@ struct ChupaText
 struct ChupaTextClass
 {
     GObjectClass parent_class;
-    void (*ready)(ChupaText *chupar,
-                  GInputStream *input);
 };
 
 GType chupa_text_get_type(void) G_GNUC_CONST;
-ChupaText *chupa_text_new(GInputStream *source);
-int chupa_text_foreach(ChupaText *chupar, GCallback *func, gpointer arg);
+ChupaText *chupa_text_new(void);
+void chupa_text_feed(ChupaText *chupar, GInputStream *stream);
+guint chupa_text_connect_decomposed(ChupaText *chupar, GCallback *func, gpointer arg);
 
 #endif
