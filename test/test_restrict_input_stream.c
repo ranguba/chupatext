2e71bbc025ba9e7c88bdc2763f3b89f1e1db4054
Nobuyoshi Nakada <nobu@ruby-lang.org>
2010-08-24 14:55:50 +0900

    add test_read_line.

--- test/test_restrict_input_stream.c
+++ test/test_restrict_input_stream.c
@@ -10,7 +10,7 @@
 void test_read (void);
 void test_skip_to_end (void);
 
-static ChupaRestrictInputStream *stream;
+static GInputStream *stream;
 
 void
 setup (void)
@@ -34,11 +34,13 @@ test_read (void)
     GInputStream *region;
     gsize count;
 
+    stream = source;
     count = g_input_stream_read(source, buf, 3, NULL, NULL);
     gcut_assert_equal_int64(3, count);
     buf[count] = '\0';
     cut_assert_equal_string("foo", buf);
     region = chupa_restrict_input_stream_new(source, 3);
+    stream = G_INPUT_STREAM(region);
     count = g_input_stream_read(region, buf, sizeof(buf), NULL, NULL);
     gcut_assert_equal_int64(3, count);
     buf[count] = '\0';
@@ -58,11 +60,13 @@ test_skip_to_end (void)
     GInputStream *region;
     gsize count;
 
+    stream = source;
     count = g_input_stream_read(source, buf, 3, NULL, NULL);
     gcut_assert_equal_int64(3, count);
     buf[count] = '\0';
     cut_assert_equal_string("foo", buf);
     region = chupa_restrict_input_stream_new(source, 6);
+    stream = G_INPUT_STREAM(region);
     count = g_input_stream_read(region, buf, 3, NULL, NULL);
     gcut_assert_equal_int64(3, count);
     buf[count] = '\0';
@@ -73,6 +77,36 @@ test_skip_to_end (void)
     gcut_assert_equal_int64(3, count);
     buf[count] = '\0';
     cut_assert_equal_string("zot", buf);
+}
+
+void
+test_read_line (void)
+{
+    static const char data[] = "foo\nbar\nzot\n";
+    char buf[sizeof(data)];
+    GInputStream *source = g_memory_input_stream_new_from_data(data, strlen(data), NULL);
+    GDataInputStream *reader;
+    gsize length;
+    char *ptr;
+
+    stream = source;
+    stream = G_INPUT_STREAM(chupa_restrict_input_stream_new(source, 5));
+    reader = g_data_input_stream_new(stream);
+    stream = G_INPUT_STREAM(reader);
+    ptr = g_data_input_stream_read_line(reader, &length, NULL, NULL);
+    gcut_assert_equal_int64(3, length);
+    cut_assert_equal_string("foo", ptr);
+    g_free(ptr);
+    ptr = g_data_input_stream_read_line(reader, &length, NULL, NULL);
+    gcut_assert_equal_int64(1, length);
+    cut_assert_equal_string("b", ptr);
+    g_free(ptr);
+    length = g_input_stream_read(source, buf, sizeof(buf), NULL, NULL);
+    gcut_assert_equal_int64(7, length);
+    buf[length] = '\0';
+    cut_assert_equal_string("ar\nzot\n", buf);
+    length = g_input_stream_read(source, buf, sizeof(buf), NULL, NULL);
+    gcut_assert_equal_int64(0, length);
 }
 
 /*
