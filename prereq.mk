3144f284f83003b6d5b8ce27bbc1f00f6cf15b24
Nobuyoshi Nakada <nobu@ruby-lang.org>
2010-09-02 18:38:47 +0900

    * prereq.mk: split source updating stuff.

---
+++ prereq.mk
@@ -0,0 +1,12 @@
+GIT = git
+
+up: pull update-version.h
+
+pull:
+	@cd "$(srcdir)" && $(GIT) pull
+
+update-version.h:
+	@cd "$(top_srcdir)" && \
+	$(SHELL) ./tool/version-update.sh chupatext/version.h
+
+.PHONY: update-version.h
