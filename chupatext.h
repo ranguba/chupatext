82d29d918ea9d398cf759b8557d47379616e81f8
Nobuyoshi Nakada <nobu@ruby-lang.org>
2010-09-13 16:02:22 +0900

    * chupatext.h: include chupa_text.h.

--- chupatext.h
+++ chupatext.h
@@ -6,11 +6,17 @@
 #ifndef CHUPATEXT_H
 #define CHUPATEXT_H
 
+#include <chupatext/chupa_text.h>
+
+G_BEGIN_DECLS
+
 int chupa_init(void *);
 int chupa_cleanup(void);
 
 const int chupa_version(void);
 const int chupa_commits(void);
 const int chupa_release_date(void);
+
+G_END_DECLS
 
 #endif  /* CHUPATEXT_H */
