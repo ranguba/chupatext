b2423e3c11e3ef14161c3c995373dfd793f3c3dc
Kouhei Sutou <kou@clear-code.com>
2010-09-26 23:25:34 +0900

    Tar decomposer works as dynamic decomposer.

--- module/tar.c
+++ module/tar.c
@@ -10,25 +10,25 @@
 
 #define CHUPA_TYPE_TAR_DECOMPOSER            chupa_type_tar_decomposer
 #define CHUPA_TAR_DECOMPOSER(obj)            \
-  G_TYPE_CHECK_INSTANCE_CAST(obj, CHUPA_TYPE_TAR_DECOMPOSER, ChupaTARDecomposer)
+  G_TYPE_CHECK_INSTANCE_CAST(obj, CHUPA_TYPE_TAR_DECOMPOSER, ChupaTarDecomposer)
 #define CHUPA_TAR_DECOMPOSER_CLASS(klass)    \
-  G_TYPE_CHECK_CLASS_CAST(klass, CHUPA_TYPE_TAR_DECOMPOSER, ChupaTARDecomposerClass)
+  G_TYPE_CHECK_CLASS_CAST(klass, CHUPA_TYPE_TAR_DECOMPOSER, ChupaTarDecomposerClass)
 #define CHUPA_IS_TAR_DECOMPOSER(obj)         \
   G_TYPE_CHECK_INSTANCE_TYPE(obj, CHUPA_TYPE_TAR_DECOMPOSER)
 #define CHUPA_IS_TAR_DECOMPOSER_CLASS(klass) \
   G_TYPE_CHECK_CLASS_TYPE(klass, CHUPA_TYPE_TAR_DECOMPOSER)
 #define CHUPA_TAR_DECOMPOSER_GET_CLASS(obj)  \
-  G_TYPE_INSTANCE_GET_CLASS(obj, CHUPA_TYPE_TAR_DECOMPOSER, ChupaTARDecomposerClass)
+  G_TYPE_INSTANCE_GET_CLASS(obj, CHUPA_TYPE_TAR_DECOMPOSER, ChupaTarDecomposerClass)
 
-typedef struct _ChupaTARDecomposer ChupaTARDecomposer;
-typedef struct _ChupaTARDecomposerClass ChupaTARDecomposerClass;
+typedef struct _ChupaTarDecomposer ChupaTarDecomposer;
+typedef struct _ChupaTarDecomposerClass ChupaTarDecomposerClass;
 
-struct _ChupaTARDecomposer
+struct _ChupaTarDecomposer
 {
     ChupaArchiveDecomposer parent_object;
 };
 
-struct _ChupaTARDecomposerClass
+struct _ChupaTarDecomposerClass
 {
     ChupaArchiveDecomposerClass parent_class;
 };
@@ -36,58 +36,48 @@ struct _ChupaTARDecomposerClass
 static GType chupa_type_tar_decomposer = 0;
 
 static void
-chupa_tar_decomposer_init(ChupaTARDecomposer *dec)
+chupa_tar_decomposer_init(ChupaTarDecomposer *dec)
 {
 }
 
 static void
-chupa_tar_decomposer_class_init(ChupaTARDecomposerClass *klass)
+chupa_tar_decomposer_class_init(ChupaTarDecomposerClass *klass)
 {
     ChupaArchiveDecomposerClass *dec_class = CHUPA_ARCHIVE_DECOMPOSER_CLASS(klass);
     dec_class->get_infile = gsf_infile_tar_new;
 }
 
-static GType
+static void
 register_type(GTypeModule *type_module)
 {
     static const GTypeInfo info = {
-        sizeof(ChupaTARDecomposerClass),
+        sizeof(ChupaTarDecomposerClass),
         (GBaseInitFunc) NULL,
         (GBaseFinalizeFunc) NULL,
         (GClassInitFunc) chupa_tar_decomposer_class_init,
         NULL,           /* class_finalize */
         NULL,           /* class_data */
-        sizeof(ChupaTARDecomposer),
+        sizeof(ChupaTarDecomposer),
         0,
         (GInstanceInitFunc) NULL,
     };
-    GType type = chupa_type_tar_decomposer;
 
-    if (!type) {
-        type = g_type_module_register_type(type_module,
-                                           CHUPA_TYPE_ARCHIVE_DECOMPOSER,
-                                           "ChupaTARDecomposer",
-                                           &info, 0);
-        chupa_type_tar_decomposer = type;
-        chupa_decomposer_register("application/tar", type);
-        chupa_decomposer_register("application/compressed-tar", type);
-    }
-    return type;
+    chupa_type_tar_decomposer =
+        g_type_module_register_type(type_module,
+                                    CHUPA_TYPE_ARCHIVE_DECOMPOSER,
+                                    "ChupaTarDecomposer",
+                                    &info, 0);
 }
 
 G_MODULE_EXPORT GList *
 CHUPA_MODULE_IMPL_INIT(GTypeModule *type_module)
 {
-    GType type = register_type(type_module);
     GList *registered_types = NULL;
 
-#if 0
-    if (type) {
-        registered_types =
-            g_list_prepend(registered_types,
-                           (gchar *)g_type_name(type));
-    }
-#endif
+    register_type(type_module);
+    registered_types =
+        g_list_prepend(registered_types,
+                       (gchar *)g_type_name(chupa_type_tar_decomposer));
 
     return registered_types;
 }
