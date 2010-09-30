9b5960ab12245923f80cd3f2b4b372f558a78d60
Nobuyoshi Nakada <nobu@ruby-lang.org>
2010-09-30 10:01:09 +0900

    * test/test_ruby.c: test for ruby module.

---
+++ test/test_ruby.c
@@ -0,0 +1,43 @@
+/* -*- Mode: C; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ *  Copyright (C) 2010  Nobuyoshi Nakada <nakada@clear-code.com>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *  MA  02110-1301  USA
+ */
+
+#include "chupa_test_util.h"
+
+void test_decompose_ruby(void);
+
+void
+setup(void)
+{
+    chupa_test_setup();
+}
+
+void
+teardown(void)
+{
+    chupa_test_teardown();
+}
+
+#define decompose_fixture chupa_test_decompose_fixture
+
+void
+test_decompose_ruby(void)
+{
+    cut_assert_equal_string("#! /usr/bin/ruby\nputs \"Hello, world\"\n", decompose_fixture("sample.rb", NULL));
+}
