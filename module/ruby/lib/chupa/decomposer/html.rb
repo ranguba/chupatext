736621e748e7488d1c130f68ff02614bd703208d
Nobuyoshi Nakada <nobu@ruby-lang.org>
2010-10-15 10:46:17 +0900

    * module/ruby/lib/chupa/decomposer/html.rb (Chupa::HTML#decompose):   wrap with StringIO to get rid of a minor bug in htmlReadMemory(),   which returns unconverted char before meta charset.

--- module/ruby/lib/chupa/decomposer/html.rb
+++ module/ruby/lib/chupa/decomposer/html.rb
@@ -16,10 +16,11 @@
 # MA  02110-1301  USA
 
 require 'nokogiri'
+require 'stringio'
 
 class Chupa::HTML < Chupa::BaseDecomposer
   def decompose
-    doc = Nokogiri::HTML.parse(read, nil, 'ASCII-8BIT')
+    doc = Nokogiri::HTML.parse(StringIO.new(read, 'rb'))
     self.metadata["title"] = (doc % "head/title").text
     if encoding = doc.encoding
       self.metadata["charset"] = encoding.downcase
