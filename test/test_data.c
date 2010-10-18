6c5b73916d9bb6112871ef62aa53d48cfc9cef5e
Yuto Hayamizu <y.hayamizu@gmail.com>
2010-10-18 17:24:47 +0900

    renamed test test_text_input -> test_data

--- test/test_data.c
+++ test/test_data.c
@@ -18,26 +18,32 @@
  *  MA  02110-1301  USA
  */
 
-#include <chupatext/chupa_text_input.h>
+#include <gsf/gsf-input-memory.h>
+#include <chupatext/chupa_data.h>
 
 #include <gcutter.h>
 
 void test_new (void);
 void test_new_with_metadata (void);
+void test_is_text (void);
 
-static ChupaTextInput *input;
+static ChupaData *data;
+static GsfInput *memory_input;
 
 void
 setup (void)
 {
-    input = NULL;
+    data = NULL;
+    memory_input = NULL;
 }
 
 void
 teardown (void)
 {
-    if (input)
-        g_object_unref(input);
+    if (data)
+        g_object_unref(data);
+    if (memory_input)
+        g_object_unref(memory_input);
 }
 
 void
@@ -45,8 +51,8 @@ test_new (void)
 {
     ChupaMetadata *meta;
     GInputStream *memstream = g_memory_input_stream_new();
-    input = chupa_text_input_new_from_stream(NULL, memstream, NULL);
-    meta = chupa_text_input_get_metadata(input);
+    data = chupa_data_new_from_stream(NULL, memstream, NULL);
+    meta = chupa_data_get_metadata(data);
     cut_assert_not_null(meta);
     cut_assert_equal_int(1, chupa_metadata_size(meta));
     cut_assert_not_null(chupa_metadata_get_first_value(meta, "mime-type"));
@@ -57,9 +63,27 @@ test_new_with_metadata (void)
 {
     ChupaMetadata *meta = chupa_metadata_new();
     GInputStream *memstream = g_memory_input_stream_new();
-    input = chupa_text_input_new_from_stream(meta, memstream, NULL);
-    gcut_assert_equal_object(meta, chupa_text_input_get_metadata(input));
+    data = chupa_data_new_from_stream(meta, memstream, NULL);
+    gcut_assert_equal_object(meta, chupa_data_get_metadata(data));
     cut_assert_equal_uint(1, chupa_metadata_size(meta));
+}
+
+void
+test_is_text (void)
+{
+    const char text_data[] = "plain\n\0text\nfoo\0bar";
+    ChupaMetadata *meta;
+    ChupaData *data;
+
+    memory_input = GSF_INPUT(gsf_input_memory_new((guint8 const *) text_data,
+                                                  sizeof(text_data) - 1,
+                                                  FALSE));
+    meta = chupa_metadata_new();
+    data = chupa_data_new(meta, memory_input);
+
+    cut_assert_false(chupa_data_is_text(data));
+    chupa_data_set_mime_type(data, "text/plain");
+    cut_assert_true(chupa_data_is_text(data));
 }
 
 /*
