d4db3f572aaefe4c5610ab601c73ffa7ffd6e9a2
Yuto Hayamizu <y.hayamizu@gmail.com>
2010-10-25 15:11:01 +0900

    prepare openoffice export scripts in CHUPA_HOME

--- module/ruby/lib/chupa/decomposer/powerpoint.rb
+++ module/ruby/lib/chupa/decomposer/powerpoint.rb
@@ -21,14 +21,74 @@ module Chupa
   class PowerPoint < BaseDecomposer
     mime_types "application/vnd.ms-powerpoint"
 
+    CHUPA_HOME = Pathname.new(File.expand_path("~/.chupa"))
+    OOFFICE_EXPORT_SCRIPT_DIR = CHUPA_HOME + ".openoffice.org/3/user/basic/Standard"
+
+    def prepare_openoffice_environment
+      unless File.directory?(OOFFICE_EXPORT_SCRIPT_DIR)
+        id = Object.new.object_id.to_s
+        ooffice_pid = spawn({"HOME" => CHUPA_HOME.to_s},
+                            "ooffice", "-headless", "macro:///Stop", id)
+        while (ps_str = `ps aux`).include?(id)
+          sleep(0.5)
+        end
+      end
+
+      script_dir = OOFFICE_EXPORT_SCRIPT_DIR
+      scripts = [{
+                   :name => "script.xlb",
+                   :content => <<EOS
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE library:library PUBLIC "-//OpenOffice.org//DTD OfficeDocument 1.0//EN" "library.dtd">
+<library:library xmlns:library="http://openoffice.org/2000/library" library:name="Standard" library:readonly="false" library:passwordprotected="false">
+ <library:element library:name="Export"/>
+</library:library>
+EOS
+                 },
+                 {
+                   :name => "Export.xba",
+                   :content => <<EOS
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE script:module PUBLIC "-//OpenOffice.org//DTD OfficeDocument 1.0//EN" "module.dtd">
+<script:module xmlns:script="http://openoffice.org/2000/script" script:name="Export" script:language="StarBasic">
+sub WritePDF(url as string)
+dim document   as object
+dim dispatcher as object
+document   = ThisComponent.CurrentController.Frame
+dispatcher = createUnoService(&quot;com.sun.star.frame.DispatchHelper&quot;)
+
+dim args1(1) as new com.sun.star.beans.PropertyValue
+args1(0).Name = &quot;URL&quot;
+args1(0).Value = url
+args1(1).Name = &quot;FilterName&quot;
+args1(1).Value = &quot;writer_pdf_Export&quot;
+
+dispatcher.executeDispatch(document, &quot;.uno:ExportDirectToPDF&quot;, &quot;&quot;, 0, args1())
+
+document.close(true)
+
+end sub
+</script:module>
+EOS
+                 }]
+      scripts.each do |script|
+        File.open(script_dir + script[:name], "w") do |file|
+          file.puts(script[:content])
+        end
+      end
+    end
+
     def decompose
+      prepare_openoffice_environment()
+
       pdf = Tempfile.new(["chupadata-pdf", ".pdf"])
       FileUtils.rm(pdf.path)
       ppt = Tempfile.new(["chupadata-ppt", ".ppt"])
       ppt.write(@source.read)
       ppt.close
 
-      ooffice_pid = spawn("ooffice", "-headless", ppt.path,
+      ooffice_pid = spawn({"HOME" => CHUPA_HOME.to_s},
+                          "ooffice", "-headless", ppt.path,
                           "macro:///Standard.Export.WritePDF(\"file://#{pdf.path}\")")
       Process.waitpid(ooffice_pid)
       ooffice_start_time = Time.now
