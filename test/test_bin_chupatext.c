12b4e737098e049ae7fa0b1d388b69738f488908
Kouhei Sutou <kou@clear-code.com>
2010-11-06 12:35:27 +0900

    cleanup.

--- test/test_bin_chupatext.c
+++ test/test_bin_chupatext.c
@@ -26,13 +26,12 @@ void test_html(void);
 void test_excel(void);
 void test_pdf_multi_pages(void);
 
-static GCutProcess *chupatext_process;
-static GString *output_from_chupatext;
+static GCutProcess *chupatext;
 
 void
 setup (void)
 {
-    chupatext_process = NULL;
+    chupatext = NULL;
     chupa_test_setup();
 }
 
@@ -40,50 +39,38 @@ void
 teardown (void)
 {
     chupa_test_teardown();
-    if (chupatext_process)
-        g_object_unref(chupatext_process);
-    if (output_from_chupatext)
-        g_string_free(output_from_chupatext, TRUE);
+    if (chupatext)
+        g_object_unref(chupatext);
 }
 
-static GCutProcess *
-start(GCutProcess *process)
+static const gchar *
+run(GCutProcess *process)
 {
-    chupatext_process = process;
+    GError *error = NULL;
+
     gcut_process_set_forced_termination_wait_time(process, 8000);
-    if (!gcut_process_run(process, NULL))
-        return NULL;
-    gcut_process_wait(process, 6000, NULL);
-    return process;
+    gcut_process_run(process, &error);
+    gcut_assert_error(error,
+                      cut_message("error message: <%s>",
+                                  gcut_process_get_error_string(process)->str));
+    gcut_process_wait(process, 6000, &error);
+    gcut_assert_error(error,
+                      cut_message("error message: <%s>",
+                                  gcut_process_get_error_string(process)->str));
+    return gcut_process_get_output_string(process)->str;
 }
 
 #define CHUPATEXT_COMMAND "./chupatext/chupatext"
 
 #define TAKE_STRING(str) cut_take_string(str)
 #define FIXTURE(fixture) TAKE_STRING(cut_build_fixture_data_path(fixture, NULL))
-static GString *
-trim_newline(GString *string)
-{
-    if (string) {
-        gsize len = string->len;
-        while (len > 0 && string->str[len - 1] == '\n')
-            --len;
-        if (++len < string->len) {
-            g_string_set_size(string, len);
-        }
-    }
-    return string;
-}
-
-#define output_string(process) trim_newline(gcut_process_get_output_string(start(process)))
-
 
 void
 test_html(void)
 {
     const gchar *path = FIXTURE("sample.html");
-    GCutProcess *process = gcut_process_new(CHUPATEXT_COMMAND, path, NULL);
-    GString *result = output_string(process);
+
+    chupatext = gcut_process_new(CHUPATEXT_COMMAND, path, NULL);
     cut_assert_equal_string("URI: sample.html\n"
                             "Content-Type: text/plain; charset=UTF-8\n"
                             "Original-Content-Length: 120\n"
@@ -95,16 +82,16 @@ test_html(void)
                             " filename=sample.html;"
                             " size=120\n"
                             "\n"
-                            "This is a sample.\n",
-                            result->str);
+                            "This is a sample.",
+                            run(chupatext));
 }
 
 void
 test_excel(void)
 {
     const gchar *path = FIXTURE("sample.xls");
-    GCutProcess *process = gcut_process_new(CHUPATEXT_COMMAND, path, NULL);
-    GString *result = output_string(process);
+
+    chupatext = gcut_process_new(CHUPATEXT_COMMAND, path, NULL);
     cut_assert_equal_string("URI: sample.xls\n"
                             "Content-Type: text/plain; charset=UTF-8\n"
                             "Original-Content-Length: 5632\n"
@@ -116,15 +103,15 @@ test_excel(void)
                             " size=5632\n"
                             "\n"
                             "sample\n1\n2\n3\n4\n5\n6\n7\n",
-                            result->str);
+                            run(chupatext));
 }
 
 void
 test_pdf_multi_pages(void)
 {
     const gchar *path = FIXTURE("sample_multi_pages.pdf");
-    GCutProcess *process = gcut_process_new(CHUPATEXT_COMMAND, path, NULL);
-    GString *result = output_string(process);
+
+    chupatext = gcut_process_new(CHUPATEXT_COMMAND, path, NULL);
     cut_assert_equal_string("URI: sample_multi_pages.pdf\n"
                             "Content-Type: text/plain; charset=UTF-8\n"
                             "Original-Content-Length: 6145\n"
@@ -138,8 +125,8 @@ test_pdf_multi_pages(void)
                             "\n"
                             "page1\n\f"
                             "2 ページ目\n\f"
-                            "page3\n",
-                            result->str);
+                            "page3",
+                            run(chupatext));
 }
 
 /*
