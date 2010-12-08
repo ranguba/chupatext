cf7609ec57fa64f0f5f5e74282c8611558a6728b
Yuto Hayamizu <y.hayamizu@gmail.com>
2010-12-09 03:57:54 +0900

    use aclocal.m4 to use macro files in non-regular dirs

--- autogen.sh
+++ autogen.sh
@@ -14,4 +14,5 @@ run svn export --force $cutter_svn_repository/cutter/trunk/misc
 
 mkdir -p m4
 run intltoolize --copy --force
+run aclocal --output acinclude.m4 ${ACLOCAL_ARGS}
 run ${AUTORECONF:-autoreconf} --install --force
